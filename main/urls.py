ab74bed73a403911d1d30d0a0c4e79046d9d1a40
Karen <ka.spk27@gmail.com>
2022-10-25 16:25:29 +0300

    11

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ app_name = 'main'
 urlpatterns = [
     path('',  views.main, name = 'main'),
     path('register',  views.register, name = 'register'),
-    path('recaptcha_v2/', views.recaptcha_v2, name = 'recaptcha_v2'),
-    path('list/', views.list, name = 'list'),
+    path('recaptcha_v2', views.recaptcha_v2, name = 'recaptcha_v2'),
+    path('list', views.list, name = 'list'),
 ]
 
