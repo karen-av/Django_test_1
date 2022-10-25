dffa25874f43905b761637118d28f9374df08c84
Karen <ka.spk27@gmail.com>
2022-10-25 14:45:32 +0300

    1

--- django_test_1/urls.py
+++ django_test_1/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main import views
 
 urlpatterns = [
+    #path('main', include('main.urls')),
+    path('',  views.index, name = 'index'),
+    #path('main/',  views.main, name = 'main'),
+    #path('main/start',  views.start, name = 'start'),
     path('admin/', admin.site.urls),
 ]
